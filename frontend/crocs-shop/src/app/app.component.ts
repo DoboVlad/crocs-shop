bb452064e5cc2f8cb03a404d4f4aa1caf9fd2b8e
Dobocan <vlad.dobocan@siemens.com>
2022-05-03 10:06:42 +0300

    Adds crocs products

--- frontend/crocs-shop/src/app/app.component.ts
+++ frontend/crocs-shop/src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crocs-shop';
+
+  crocs = [
+    {
+      img: 'image test',
+      name: 'Yellow crocs',
+      price: '40$'
+    },
+    {
+      img: 'image test',
+      name: 'Blue crocs',
+      price: '50$'
+    },
+    {
+      img: 'image test',
+      name: 'Red crocs',
+      price: '60$'
+    }
+  ]
 }
