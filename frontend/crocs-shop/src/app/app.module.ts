81dcda51b35c843b215822e179555b4a2517fcb7
anamarias97 <anamaria97s@yahoo.com>
2022-05-18 15:51:11 +0300

    added footer and home page draft

--- frontend/crocs-shop/src/app/app.module.ts
+++ frontend/crocs-shop/src/app/app.module.ts
@@ -5,14 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { CrocsDetailsComponent } from './crocs-details/crocs-details.component';
+import { CrocsDetailsComponent } from './components/crocs-details/crocs-details.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './core/login/login.component';
+import { RegisterComponent } from './core/register/register.component';
+import { AccountComponent } from './pages/account/account.component';
+import { ProductsComponent } from './pages/products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    CrocsDetailsComponent
+    CrocsDetailsComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    AccountComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
